8aeacd4bb384bcbedfb79f1d0c32fc895e32f601
noahj706 <noahj706@gmail.com>
2026-04-23 20:13:40 -0700

    raylib link fixes

--- CrazyOnCode_PA9/entity.hpp
+++ CrazyOnCode_PA9/entity.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "raylib.h"
+
 class entity
 {
+	
 
 };
---
+++ CrazyOnCode_PA9/main.cpp
@@ -0,0 +1,13 @@
+/*
+Programmer: Noah Julius, Angelo Reyes, Walker [LASTNAME], Lincoln [LASTNAME]
+Title: Atari Combat Recreation / PA9
+Description: Local 2 player pvp with tanks, first to 15 wins
+*/
+
+#include "raylib.h"
+
+
+int main(void)
+{
+   
+}
