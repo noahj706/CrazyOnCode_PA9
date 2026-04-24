52c3ef3a6dc9854ec778081ba942e42c633db8b8
noahj706 <noahj706@gmail.com>
2026-04-24 15:11:33 -0700

    initial entity code implamented

---
+++ CrazyOnCode_PA9/entity.cpp
@@ -0,0 +1,24 @@
+#include "entity.hpp"
+
+void Entity::updateVelocity()//basically polar to cartesian coords
+{
+	velocity = {speed * cosf(angle), speed * sinf(angle)};
+}
+Entity::Entity(Vector2 position = { 0,0 }, float angle = 0, float speed = 0)//constructor
+{
+	this->position = position;
+	this->angle = angle;
+	this->speed = speed;
+}
+
+void Entity::moveForward()//uses updateVelocity() and then adds it to position
+{
+	updateVelocity();
+	position = Vector2Add(position, velocity);
+}
+void Entity::moveBackward()//subtracts vector from position as a halved rate
+{
+	updateVelocity();
+	Vector2Scale(velocity, BACKWARD_SPEED);//backward speed adjustable in the header file
+	position = Vector2Subtract(position, velocity);
+}
--- CrazyOnCode_PA9/entity.hpp
+++ CrazyOnCode_PA9/entity.hpp
@@ -1,9 +1,27 @@
 #pragma once
 
 #include "raylib.h"
+#include "raymath.h"
 
-class entity
+#define BACKWARD_SPEED 0.5 //how much % slower smth moves backwards
+
+class Entity
 {
-	
+	Vector2 velocity; //combines angle and speed into a single vector, this is then added to position for movement
+	void updateVelocity();//basically polar to cartesian coords
 
+public:		
+	Vector2 position;
+	float angle; //will be stored as deg, getters can pass as deg or radians with DEG2RAD from raylib
+	float speed; //distance added to position for movement
+
+	Entity(Vector2 position = {0,0}, float angle = 0, float speed = 0);//constructor
+
+	virtual ~Entity() = default;//virtual destructor
+
+	virtual void update() = 0; //pure virtual function, different for whatever inherits
+	virtual void draw() = 0; //pure virtual function, this'll be the render step for the object
+	
+	void moveForward();//uses updateVelocity() and then adds it to position
+	void moveBackward();//subtracts vector from position as a halved rate
 };
