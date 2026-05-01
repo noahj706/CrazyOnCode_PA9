957b22b357aac4a61b6f550125e926d1b60d4636
noahj706 <noahj706@gmail.com>
2026-05-01 05:46:54 -0700

    fi

--- CrazyOnCode_PA9/UI.hpp
+++ CrazyOnCode_PA9/UI.hpp
@@ -2,7 +2,7 @@
 #include "entity.hpp"
 #include <string>
 
-#define WINNING_SCORE 1
+#define WINNING_SCORE 3
 constexpr const char* FIRE_SOUND_PATH = "smoke.wav";
 constexpr const char* DESTROYED_SOUND_PATH = "destroyed.wav";
 
